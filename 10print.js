e0d129caaa1bbe3a05291b5ad44fca28f6baafe6
cdk <cdklinck@gmail.com>
2022-02-25 16:48:58 -0600

    cyan blocks

--- 10print.js
+++ 10print.js
@@ -2,16 +2,26 @@
   one possible implemenation of the 10print.org algorithm written in JavaScript
 */
 
-const w = process.stdout.columns
+const sw = process.stdout.columns
+let w = 0
+let d = 1
 
 function draw () {
-  setTimeout(draw, 1000)
+  setTimeout(draw, 1000/12)
+
+  if (w > sw - 1 || w < 0) {
+    d = -d
+  }
+
+  w += d
+
+
   let output = ''
   for (let i = 0; i < w; i++) {
     if (Math.random() > 0.5) {
-      output += '<3'
+      output += '\x1b[36m' + '░'
     } else {
-      output += '/'
+      output += '\x1b[36m' + '▒'
     }
   }
   console.log(output)
